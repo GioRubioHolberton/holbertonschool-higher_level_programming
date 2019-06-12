9f3ebf05d53d73474995bea551381b9604133c5c
GioRubioHolberton <759@holbertonschool.com>
2019-06-12 03:16:30 +0000

    Try correction base.py file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+"""Base Class"""
 import json
 
 
 class Base:
-    """base class"""
     __nb_objects = 0
 
     def __init__(self, id=None):
@@ -12,18 +12,3 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
-
-    @staticmethod
-    def to_json_string(list_dictionaries):
-        if list_dictionaries is None or len(list_dictionaries) == 0:
-            return("[]")
-        return json.dumps(list_dictionaries)
-
-    @classmethod
-    def save_to_file(cls, list_objs):
-        new_list = []
-        if list_objs:
-            for li_obj in list_objs:
-                new_list.append(li_obj.to_dictionary())
-        with open(cls.__name__ + '.json', 'w') as file:
-            file.write(cls.to_json_string(new_list))
