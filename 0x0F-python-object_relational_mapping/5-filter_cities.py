1e56001d3f40938975310111904e0f2566276bbf
GioRubioHolberton <759@holbertonschool.com>
2019-08-11 19:09:55 +0000

    All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     cur = db.cursor()
     cur.execute("""SELECT cities.name FROM states
                 JOIN cities ON cities.state_id = states.id
-                WHERE states.name = %s""", (sys.argv[4],))
+                WHERE states.name = %s""", (argv[4],))
     rows = cur.fetchall()
     city = 0
     for row in rows:
