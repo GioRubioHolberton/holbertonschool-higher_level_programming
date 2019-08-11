66c734f06674f27ec466d1eebdfaea5715134cc5
GioRubioHolberton <759@holbertonschool.com>
2019-08-11 17:58:56 +0000

    SQL Injection...

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" script that takes in an argument and displays all values """
+
+if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE name= %s
+                ORDER BY id ASC""".format(argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
