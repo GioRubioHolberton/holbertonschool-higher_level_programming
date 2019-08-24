72a4a2d3ef3db242cd57429d92e17b6a312e667f
GioRubioHolberton <759@holbertonschool.com>
2019-08-24 04:35:29 +0000

    Exercises 2-9

---
+++ 0x10-python-network_0/100-status_code.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# function that finds a peak in a list of unsorted integers.
+curl -so %{http_code} $1 -w /dev/null
---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,3 @@
+#!/bin/bash
+# function that finds a peak in a list of unsorted integers.
+curl -sX POST $1 -d 'email=hr@holbertonschool.com&subject=I will always be here for PLD'
