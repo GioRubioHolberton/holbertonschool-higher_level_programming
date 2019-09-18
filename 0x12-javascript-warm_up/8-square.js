0db255cc28c544268df93638ee81063c0f45f04f
GioRubioHolberton <759@holbertonschool.com>
2019-09-18 03:02:50 +0000

    task 8 correction

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,11 +3,11 @@ const myC = parseInt(process.argv[2]);
 if (isNaN(myC)) {
   console.log('Missing size');
 } else {
-    for (let i = 0; i < myC; i++) {
-      let myX = '';
+  for (let i = 0; i < myC; i++) {
+    let myX = '';
     for (let j = 0; j < myC; j++) {
-      myX = 'X';
+      myX += 'X';
     }
-    console.log('C is fun');
+    console.log(myX);
   }
 }
