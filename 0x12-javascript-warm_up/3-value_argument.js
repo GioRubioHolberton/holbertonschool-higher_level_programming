f328ea4926c631c93e19de65ad1b5eeabaf01bfc
GioRubioHolberton <759@holbertonschool.com>
2019-09-18 02:15:30 +0000

    semistandard

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const my_argv = process.argv;
-if (my_argv[2]) {
-  console.log(my_argv[2]);
+const myargv = process.argv;
+if (myargv[2]) {
+  console.log(myargv[2]);
 } else {
   console.log('No argument');
 }
