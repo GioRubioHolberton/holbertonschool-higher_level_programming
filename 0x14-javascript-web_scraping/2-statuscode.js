70c9acad23d9a524bae61a378abfaff2473e69b8
GioRubioHolberton <759@holbertonschool.com>
2019-09-25 02:29:25 +0000

    Tasks 1, 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const url = process.argv[2]; 
+const req = require('request');
+req(url, function (err, resp) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + resp.statusCode);
+  }
+});
